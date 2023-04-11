3a9467d390d9d8849aeacf210c15916fd4d01cb6
Rigoberto Calderon <jesuscalflo15@gmail.com>
2023-04-11 00:00:21 -0600

    Se agregaron sout para mostrar diferentes tipos de datos

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     /**
      * Funcion principal de entrada de la aplicación
-     * @param args
+     * @param args, recibe los argumentos
      */
     public static void main(String[] args) {
         // Este es un comentario, de una sola línea
@@ -15,8 +15,13 @@ public class Main {
         Aquí es un texto largo, como comentario
         */
 
-        // Imprimiendo mensaje en consola
+        // Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
         System.out.println("Hola mundo");
-
+        System.out.println(1);
+        System.out.println(10L);
+        System.out.println(11.2);
+        System.out.println("Soy un string");
+        System.out.println(false);
+        System.out.println(10.0f);
     }
 }
