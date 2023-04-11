998f23baab628f6816c5f188367133e911891323
Rigoberto Calderon <jesuscalflo15@gmail.com>
2023-04-10 23:56:58 -0600

    [ADD] Agregando primeros ejercicios de ob sección 1

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * Clase principal, ejemplo java doc
+ * @author rcalderon
+ */
+public class Main {
+
+    /**
+     * Funcion principal de entrada de la aplicación
+     * @param args
+     */
+    public static void main(String[] args) {
+        // Este es un comentario, de una sola línea
+
+        /*
+        Aquí es un texto largo, como comentario
+        */
+
+        // Imprimiendo mensaje en consola
+        System.out.println("Hola mundo");
+
+    }
+}
